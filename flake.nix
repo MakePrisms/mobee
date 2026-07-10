1c8118bef63204fda2a59ce011b7dfc860a3f594
gudnuf <gudnuf21@proton.me>
2026-07-10 15:37:36 -0700

    Bootstrap distributable Mobee binary
    Establish the smallest executable Rust target with reproducible Cargo and Nix packaging so development can proceed piece by piece.
    
    Co-authored-by: gudnuf <gudnuf21@proton.me>
    Signed-off-by: gudnuf <gudnuf21@proton.me>

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Mobee";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "mobee";
+            version = "0.1.0";
+            src = self;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            meta = {
+              description = "Mobee";
+              mainProgram = "mobee";
+            };
+          };
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/mobee";
+        };
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              clippy
+              rustc
+              rustfmt
+            ];
+          };
+        }
+      );
+    };
+}
